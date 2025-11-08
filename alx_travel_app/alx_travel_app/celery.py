17e7f4ed36f5cad4ea226756df04cb4d175452b4
codefromlani <rodiathammed48@gmail.com>
2025-11-08 10:04:03 +0100

    fix: correct import of celery app

--- alx_travel_app/alx_travel_app/celery.py
+++ alx_travel_app/alx_travel_app/celery.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 import os
-from celery import Celery
+from alx_travel_app.alx_travel_app.celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx_travel_app.settings')
 
--- alx_travel_app/listings/tasks.py
+++ alx_travel_app/listings/tasks.py
@@ -1,4 +1,4 @@
-from celery import shared_task
+from alx_travel_app.alx_travel_app.celery import shared_task
 from django.core.mail import send_mail
 from django.conf import settings
 
